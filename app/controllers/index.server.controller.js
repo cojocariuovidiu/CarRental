84fd67eb661e6ff36639a63a764b570f421998e7
Abhishek Bongale <bongale.abhi97@outlook.com>
2018-03-23 22:37:27 +0530

    Session code added

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,4 +1,17 @@
 exports.render=function(req,res){
+	
+
+	//If the session's lastVisit property
+	if(req.session.lastVisit){
+		console.log(req.session.lastVisit);
+	}
+
+	//Set the session's 'lastVisit' property
+	req.session.lastVisit = new Date();
+
+
+
+	//Use the 'respoonse ' object to render the 'index' view with a 'title' property
 	res.render('index',{
 		title:'Hello World'
 	});
--- config/express.js
+++ config/express.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const session = require('express-session');
+const config = require('./config');
 
 module.exports=function(){
 	const app = express();
@@ -6,6 +8,13 @@ module.exports=function(){
 	//Set the application view engine and 'views' folder
 	app.set('views','./app/views');
 	app.set('view engine','ejs');
+
+	//Configure the 'session' middleware
+	app.use(session({
+		saveUnintialized:true,
+		resave:true,
+		secret:config.sessionSecret
+	}));
 
 	//set routes
 	require('../app/routes/index.server.routes')(app);
